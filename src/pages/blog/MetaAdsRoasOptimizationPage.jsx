7957a5a10548ce1fc87a9a31335f2352d16cba73
DayByDay VPS <contact@daybydayconsulting.com>
2026-05-25 16:01:52 +0200

    fix: MetaAdsRoasOptimizationPage component name mismatch

--- src/pages/blog/MetaAdsRoasOptimizationPage.jsx
+++ src/pages/blog/MetaAdsRoasOptimizationPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-const OptimizacionROASMetaAdsPage = () => {
+const MetaAdsRoasOptimizationPage = () => {
   const schema = {
     "@context": "https://schema.org",
     "@type": "BlogPosting",
